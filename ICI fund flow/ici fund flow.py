35dfa7038a701c6a6c0966ea29bce639d4b8f9ee
vkinvest <vkyinvest@gmail.com>
2022-02-02 10:59:22 -0500

    Update

---
+++ ICI fund flow/ici fund flow.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class ICIFundFLow:
+
+    def __init__(self):
+        self.data = pd.read_excel('mm_summary_data_2022.xls').set_index('Date')
+
+    def value(self):
+        plt.figure(figsize=(12, 6))
+        plt.plot(self.data['INSTITUTIONAL'])
+        plt.plot(self.data['RETAIL'])
+        plt.show()
+
+    def change(self):
+        plt.figure(figsize=(12, 6))
+        plt.plot(self.data['change1'])
+        plt.plot(self.data['change2'])
+        plt.show()
+
+
+ici = ICIFundFLow()
+ici.value()
+ici.change()
